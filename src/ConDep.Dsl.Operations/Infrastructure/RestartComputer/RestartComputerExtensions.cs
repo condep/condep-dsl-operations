8be8436ba3b3793a99443b597a1f6515731bc2c2
Jon Arild Torresdal <jon@torresdal.net>
2014-08-19 22:59:51 +0200

    Changed restart computer operation to use shutdown cmd instead of powershell restart-computer

---
+++ src/ConDep.Dsl.Operations/Infrastructure/RestartComputer/RestartComputerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using ConDep.Dsl.Operations.Infrastructure.RestartComputer;
+
+namespace ConDep.Dsl
+{
+    public static class RestartComputerExtensions
+    {
+        public static IOfferRemoteOperations Restart(this IOfferRemoteOperations remote, int delayInSeconds = 10)
+        {
+            var restartOperation = new RestartComputerOperation(delayInSeconds);
+            Configure.Remote(remote, restartOperation);
+            return remote;
+        }
+    }
+}
--- src/ConDep.Dsl.Operations/Infrastructure/RestartComputer/RestartComputerOperation.cs
+++ src/ConDep.Dsl.Operations/Infrastructure/RestartComputer/RestartComputerOperation.cs
@@ -4,14 +4,20 @@ using System.Net.NetworkInformation;
 using System.Threading;
 using ConDep.Dsl.Config;
 using ConDep.Dsl.Logging;
-using ConDep.Dsl.Operations;
 using ConDep.Dsl.Remote;
 using ConDep.Dsl.Validation;
 
-namespace ConDep.Dsl
+namespace ConDep.Dsl.Operations.Infrastructure.RestartComputer
 {
     public class RestartComputerOperation : RemoteOperation
     {
+        private readonly int _delayInSeconds;
+
+        public RestartComputerOperation(int delayInSeconds)
+        {
+            _delayInSeconds = delayInSeconds;
+        }
+
         public enum WaitForStatus
         {
             Success,
@@ -22,25 +28,37 @@ namespace ConDep.Dsl
             var canPingServer = CanPingServer(server);
             Logger.Verbose(string.Format("Can {0}use ping for validation", canPingServer ? "" : "NOT "));
 
-            var powershellExecutor = new PowerShellExecutor(server);
-            powershellExecutor.Execute("Restart-Computer -Force");
+            Logger.WithLogSection("Restarting", () =>
+            {
+                Logger.Info(string.Format("Executing restart command on server {0}", server.Name));
+                var powershellExecutor = new PowerShellExecutor(server);
+                powershellExecutor.Execute(string.Format("cmd /c \"shutdown /r /t {0}\"", _delayInSeconds));
 
-            if (canPingServer)
-            {
-                Logger.Verbose("Waiting for ping to fail");
-                WaitForPing(WaitForStatus.Failure, server);
-                Logger.Verbose("Waiting for ping to Succeed");
-                WaitForPing(WaitForStatus.Success, server);
-            }
-            else
-            {
-                Logger.Verbose("Waiting for WinRM to fail");
-                WaitForWinRm(WaitForStatus.Failure, server);
-            }
-            Logger.Verbose("Waiting for WinRM to succeed");
-            WaitForWinRm(WaitForStatus.Success, server);
-            Logger.Info("Computer restarted");
-            Logger.WithLogSection("Starting ConDepNode", () => ConDepNodePublisher.StartNode(server));
+                if (canPingServer)
+                {
+                    Logger.Verbose("Waiting for ping to fail");
+                    Logger.Info("Waiting for server to stop responding");
+                    WaitForPing(WaitForStatus.Failure, server);
+                    Logger.Info("Server stopped responding");
+                    Logger.Verbose("Waiting for ping to Succeed");
+                    Logger.Info("Waiting for server to respond again");
+                    WaitForPing(WaitForStatus.Success, server);
+                    Logger.Info("Server started to respond");
+                }
+                else
+                {
+                    Logger.Verbose("Waiting for WinRM to fail");
+                    Logger.Info("Waiting for server to stop responding");
+                    WaitForWinRm(WaitForStatus.Failure, server);
+                    Logger.Info("Server stopped responding");
+                }
+                Logger.Verbose("Waiting for WinRM to succeed");
+                Logger.Info("Waiting for server to respond to PowerShell commands");
+                WaitForWinRm(WaitForStatus.Success, server);
+                Logger.Info("Serve successfully responds to PowerShell commands");
+                Logger.Info("Computer successfully restarted");
+                Logger.WithLogSection("Starting ConDepNode", () => ConDepNodePublisher.StartNode(server));
+            });
         }
 
         private void WaitForWinRm(WaitForStatus status, ServerConfig server)
@@ -99,27 +117,44 @@ namespace ConDep.Dsl
 
         private void WaitForPing(WaitForStatus failure, ServerConfig server)
         {
-            var pingTool = new Ping();
-            var reply = pingTool.Send(server.Name);
-
-            switch (failure)
+            try
             {
-                case WaitForStatus.Failure:
-                    if (reply.Status == IPStatus.Success)
-                    {
+                var pingTool = new Ping();
+                var reply = pingTool.Send(server.Name);
+
+                switch (failure)
+                {
+                    case WaitForStatus.Failure:
+                        if (reply.Status == IPStatus.Success)
+                        {
+                            Thread.Sleep(1000);
+                            WaitForPing(failure, server);
+                        }
+                        break;
+                    case WaitForStatus.Success:
+                        if (reply.Status != IPStatus.Success)
+                        {
+                            Thread.Sleep(1000);
+                            WaitForPing(failure, server);
+                        }
+                        break;
+                    default:
+                        throw new Exception("Status not supported.");
+                }
+            }
+            catch
+            {
+                switch (failure)
+                {
+                    case WaitForStatus.Failure:
+                        return;
+                    case WaitForStatus.Success:
                         Thread.Sleep(1000);
                         WaitForPing(failure, server);
-                    }
-                    break;
-                case WaitForStatus.Success:
-                    if (reply.Status != IPStatus.Success)
-                    {
-                        Thread.Sleep(1000);
-                        WaitForPing(failure, server);
-                    }
-                    break;
-                default:
-                    throw new Exception("Status not supported.");
+                        break;
+                    default:
+                        throw new Exception("Status not supported.");
+                }
             }
         }
 
@@ -143,16 +178,6 @@ namespace ConDep.Dsl
         public override bool IsValid(Notification notification)
         {
             return true;
-        }
-    }
-
-    public static class RestartComputerExtensions
-    {
-        public static IOfferRemoteOperations Restart(this IOfferRemoteOperations remote)
-        {
-            var restartOperation = new RestartComputerOperation();
-            Configure.Remote(remote, restartOperation);
-            return remote;
         }
     }
 }
