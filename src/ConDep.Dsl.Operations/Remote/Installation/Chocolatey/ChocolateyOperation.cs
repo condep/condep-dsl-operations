35ea2e5a87c52cbc1d618e2c1a1521d607dc559e
Jon Arild Torresdal <jon@torresdal.net>
2015-03-07 23:08:08 +0100

    Minor code opt for handling choco options

--- src/ConDep.Dsl.Operations/Remote/Installation/Chocolatey/ChocolateyOperation.cs
+++ src/ConDep.Dsl.Operations/Remote/Installation/Chocolatey/ChocolateyOperation.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using ConDep.Dsl.Validation;
 
 namespace ConDep.Dsl.Operations.Remote.Installation.Chocolatey
@@ -43,21 +44,22 @@ namespace ConDep.Dsl.Operations.Remote.Installation.Chocolatey
                 if (options.SkipPowerShell) switches += "n";
             }
 
-            var opt = " --acceptlicense";
+            var opt = new List<string> {"--acceptlicense"};
+
             if (options != null)
             {
-                if (options.ForceX86) opt += " --x86";
-                if (options.NotSilent) opt += " --notsilent";
-                if (options.PreRelease) opt += " --pre";
-                if (!string.IsNullOrWhiteSpace(options.InstallerArgs)) opt += "--ia=\"" + options.InstallerArgs + "\"";
-                if (!string.IsNullOrWhiteSpace(options.PackageParams)) opt += "--params=\"" + options.PackageParams + "\"";
-                if (!string.IsNullOrWhiteSpace(options.Source)) opt += "--source=" + options.Source;
-                if (options.TimeoutInSeconds > 0) opt += "--execution-timeout=" + options.TimeoutInSeconds;
-                if (!string.IsNullOrWhiteSpace(options.Version)) opt += "--version=\"" + options.Version + "\"";
-                if (!string.IsNullOrWhiteSpace(options.CacheLocation)) opt += "--cache=" + "\"" + options.CacheLocation + "\"";
+                if (options.ForceX86) opt.Add("--x86");
+                if (options.NotSilent) opt.Add("--notsilent");
+                if (options.PreRelease) opt.Add("--pre");
+                if (!string.IsNullOrWhiteSpace(options.InstallerArgs)) opt.Add("--ia=\"" + options.InstallerArgs + "\"");
+                if (!string.IsNullOrWhiteSpace(options.PackageParams)) opt.Add("--params=\"" + options.PackageParams + "\"");
+                if (!string.IsNullOrWhiteSpace(options.Source)) opt.Add("--source=" + options.Source);
+                if (options.TimeoutInSeconds > 0) opt.Add("--execution-timeout=" + options.TimeoutInSeconds);
+                if (!string.IsNullOrWhiteSpace(options.Version)) opt.Add("--version=\"" + options.Version + "\"");
+                if (!string.IsNullOrWhiteSpace(options.CacheLocation)) opt.Add("--cache=" + "\"" + options.CacheLocation + "\"");
             }
 
-            return switches + opt;
+            return string.Format("{0} {1}", switches, string.Join(" ", opt));
         }
     }
 }
