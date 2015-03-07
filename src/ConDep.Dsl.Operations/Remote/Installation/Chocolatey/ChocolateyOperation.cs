28c951ddd04d3e0bf6e91912d05932cc8c3eca3d
Jon Arild Torresdal <jon@torresdal.net>
2015-03-07 19:39:02 +0100

    Fixed issue where op failed when no options where specified

--- src/ConDep.Dsl.Operations/Remote/Installation/Chocolatey/ChocolateyOperation.cs
+++ src/ConDep.Dsl.Operations/Remote/Installation/Chocolatey/ChocolateyOperation.cs
@@ -32,24 +32,30 @@ namespace ConDep.Dsl.Operations.Remote.Installation.Chocolatey
         private static string BuildOptions(ChocolateyOptionValues options)
         {
             var switches = "-y";
-            if (options.AllowMultipleVersions) switches += "m";
-            if (options.Force) switches += "f";
-            if (options.ForceDependencies) switches += "x";
-            if (options.IgnoreDependencies) switches += "i";
-            if (options.LimitOutput) switches += "r";
-            if (options.OverrideArgs) switches += "o";
-            if (options.SkipPowerShell) switches += "n";
+            if (options != null)
+            {
+                if (options.AllowMultipleVersions) switches += "m";
+                if (options.Force) switches += "f";
+                if (options.ForceDependencies) switches += "x";
+                if (options.IgnoreDependencies) switches += "i";
+                if (options.LimitOutput) switches += "r";
+                if (options.OverrideArgs) switches += "o";
+                if (options.SkipPowerShell) switches += "n";
+            }
 
             var opt = " --acceptlicense";
-            if (options.ForceX86) opt += " --x86";
-            if (options.NotSilent) opt += " --notsilent";
-            if (options.PreRelease) opt += " --pre";
-            if (!string.IsNullOrWhiteSpace(options.InstallerArgs)) opt += "--ia=\"" + options.InstallerArgs + "\"";
-            if (!string.IsNullOrWhiteSpace(options.PackageParams)) opt += "--params=\"" + options.PackageParams + "\"";
-            if (!string.IsNullOrWhiteSpace(options.Source)) opt += "--source=" + options.Source;
-            if (options.TimeoutInSeconds > 0) opt += "--execution-timeout=" + options.TimeoutInSeconds;
-            if (!string.IsNullOrWhiteSpace(options.Version)) opt += "--version=\"" + options.Version + "\"";
-            if (!string.IsNullOrWhiteSpace(options.CacheLocation)) opt += "--cache=" + "\"" + options.CacheLocation + "\"";
+            if (options != null)
+            {
+                if (options.ForceX86) opt += " --x86";
+                if (options.NotSilent) opt += " --notsilent";
+                if (options.PreRelease) opt += " --pre";
+                if (!string.IsNullOrWhiteSpace(options.InstallerArgs)) opt += "--ia=\"" + options.InstallerArgs + "\"";
+                if (!string.IsNullOrWhiteSpace(options.PackageParams)) opt += "--params=\"" + options.PackageParams + "\"";
+                if (!string.IsNullOrWhiteSpace(options.Source)) opt += "--source=" + options.Source;
+                if (options.TimeoutInSeconds > 0) opt += "--execution-timeout=" + options.TimeoutInSeconds;
+                if (!string.IsNullOrWhiteSpace(options.Version)) opt += "--version=\"" + options.Version + "\"";
+                if (!string.IsNullOrWhiteSpace(options.CacheLocation)) opt += "--cache=" + "\"" + options.CacheLocation + "\"";
+            }
 
             return switches + opt;
         }
