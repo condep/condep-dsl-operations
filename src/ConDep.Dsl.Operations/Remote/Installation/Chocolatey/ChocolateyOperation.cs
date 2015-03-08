07ea9b29e2ca779b84080fe6db27337e74fb77b2
Jon Arild Torresdal <jon@torresdal.net>
2015-03-08 15:14:48 +0100

    Added package check pre-condition

--- src/ConDep.Dsl.Operations/Remote/Installation/Chocolatey/ChocolateyOperation.cs
+++ src/ConDep.Dsl.Operations/Remote/Installation/Chocolatey/ChocolateyOperation.cs
@@ -28,7 +28,41 @@ namespace ConDep.Dsl.Operations.Remote.Installation.Chocolatey
         public override void Configure(IOfferRemoteComposition server)
         {
             var options = BuildOptions(_options);
-            server.Execute.PowerShell(string.Format("choco install {0} {1}", string.Join(" ", _packageNames), options));
+            server.Execute.PowerShell(string.Format(@"
+function choco-package-installed($name, $version = $null) {{
+    $result = choco version $($name.Trim()) -localonly
+
+    if(!$?) {{ return $false }}    
+    if(!$version) {{ return $true }}
+
+	$resultArray = ($result -split '[\r\n]') |? {{$_}}
+	$packages = @{{}}
+
+    foreach($item in $resultArray) {{
+    	$line = $item.Trim() -split ""\s+""
+    	if($line.Count -eq 2) {{
+    		$packages.Add($line[0].ToLower().Trim(), $line[1].ToLower().Trim())
+    	}}
+    }}
+
+    $packageVersion = $packages[$($name.Trim().ToLower())]
+
+    if($packageVersion -eq $null) {{ return $false }}
+
+    return $packageVersion.ToLower().Trim() -eq $version.ToLower().Trim()
+}}
+
+$packageNames = ""{0}"" -split "" ""
+
+foreach($package in $packageNames) {{
+    if(!(choco-package-installed $package)) {{
+        choco install $package {1}
+    }}
+    else {{
+        write-host ""Package $package allready installed.""
+    }}
+}}
+", string.Join(" ", _packageNames), options));
         }
 
         private static string BuildOptions(ChocolateyOptionValues options)
